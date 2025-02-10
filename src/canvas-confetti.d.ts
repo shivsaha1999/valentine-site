bf21cf88dc846e3329795368b3e517f0a180e5b1
Shivangi Saha <Shivangi.Saha@cognizant.com>
2025-02-10 11:36:07 +0530

    chore: Update canvas-confetti.d.ts with additional options

--- src/canvas-confetti.d.ts
+++ src/canvas-confetti.d.ts
@@ -1,4 +1,28 @@
 declare module 'canvas-confetti' {
-    const confetti: any;
-    export default confetti;
+    interface Options {
+      angle?: number;
+      spread?: number;
+      startVelocity?: number;
+      decay?: number;
+      gravity?: number;
+      drift?: number;
+      ticks?: number;
+      x?: number;
+      y?: number;
+      shapes?: string[];
+      zIndex?: number;
+      colors?: string[];
+      disableForReducedMotion?: boolean;
+      scalar?: number;
+      particleCount?: number;
+      origin?: { x: number; y: number };
+    }
+  
+    function confetti(options?: Options): Promise<undefined>;
+  
+    namespace confetti {
+      let Promise: PromiseLike<undefined> | null | undefined;
+    }
+  
+    export = confetti;
   }
